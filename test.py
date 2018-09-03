8cae98d9ee4f9c21915e5eda45f3da2486d04947
david1412 <davidkumar1412@gmail.com>
2018-09-03 14:10:49 +0200

    food

---
+++ test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  3 14:02:55 2018
+
+@author: Gopalam
+"""
+
+hungry = input (" are you hungry?)
+
+if hungry=="yes":
+    print("eat bread")
+    print("eat dosa")
+else:
+    print("do your work")
